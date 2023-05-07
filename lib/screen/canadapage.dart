abef5ec4adc800de508bf0ee95e4740ed1b65d8e
박현빈 <choiemhb@gmail.com>
2023-05-07 19:54:57 +0900

    test

--- lib/screen/canadapage.dart
+++ lib/screen/canadapage.dart
@@ -7,7 +7,7 @@ class CANADAPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('Canada'),
+        child: Text('Canda'),
       ),
     );
   }
