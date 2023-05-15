130301d5b1a58959ed995ecf27430235029b7d5b
박현빈 <choiemhb@gmail.com>
2023-05-15 21:37:04 +0900

    scrollview button이랑 image 연결

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:karim/screens/tipscreen.dart';
 import '../data/color_list.dart';
-import 'package:karim/widgets/recommended_apps.dart';
+import '../widgets/recommended_apps.dart';
 import '../widgets/scrollview.dart';
 
 class CountryPage extends StatefulWidget {
@@ -34,69 +34,7 @@ class _CountryPageState extends State<CountryPage> {
       body: Column(
         children: [
           const Scrollview(),
-          const SizedBox(height: 5),
-          Container(
-            width: 350,
-            height: 350,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/food.png'),
-              ),
-            ),
-            child: Stack(
-              children: [
-                Positioned(
-                  bottom: 30,
-                  left: 25,
-                  child: Container(
-                    width: 300,
-                    height: 100,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 55,
-                  left: 55,
-                  child: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage('assets/images/baemin.png'),
-                      ),
-                    ),
-                  ),
-                ),
-                const Positioned(
-                  bottom: 85,
-                  left: 120,
-                  child: Text(
-                    "BAEMIN",
-                    style: TextStyle(
-                        color: Colors.black, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                const Positioned(
-                  bottom: 65,
-                  left: 120,
-                  child: Text(
-                    "Korea's No. 1 delivery app!", //api
-                    style: TextStyle(
-                      color: Color(0xffA8A6A6),
-                      fontSize: 14,
-                      letterSpacing: 0,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
+
           GestureDetector(
             onTap: () {
               Navigator.push(context,
--- lib/widgets/scrollview.dart
+++ lib/widgets/scrollview.dart
@@ -21,33 +21,61 @@ class ScrollviewState extends State<Scrollview> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 5),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: [
-            for (String button in buttons)
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: TextButton(
-                  onPressed: () {
-                    setState(() {
-                      selectedButton = button;
-                    });
-                  },
-                  child: Text(
-                    button,
-                    style: TextStyle(
-                      color:
-                          selectedButton == button ? Colors.black : Colors.grey,
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 20, right: 20, bottom: 5),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for (String button in buttons)
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: TextButton(
+                      onPressed: () {
+                        setState(() {
+                          selectedButton = button;
+                        });
+                      },
+                      child: Text(
+                        button,
+                        style: TextStyle(
+                          color: selectedButton == button
+                              ? Colors.black
+                              : Colors.grey,
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              ),
-          ],
+              ],
+            ),
+          ),
         ),
-      ),
+        Container(
+          width: 350,
+          height: 350,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                selectedButton == "Food"
+                    ? 'assets/images/food.png'
+                    : selectedButton == "Trip"
+                        ? 'assets/images/trip.png'
+                        : selectedButton == "Shopping"
+                            ? 'assets/images/shopping.png'
+                            : selectedButton == "Financial"
+                                ? 'assets/images/financial.png'
+                                : selectedButton == "SNS"
+                                    ? 'assets/images/sns.png'
+                                    : 'assets/images/game.png',
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10),
+      ],
     );
   }
 }
