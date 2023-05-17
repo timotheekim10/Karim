1a7eb22a0fec8a2190cdd1a70ddada644ff7773e
박현빈 <choiemhb@gmail.com>
2023-05-17 14:32:16 +0900

    캐러셀 추가, image 연결 방식 수정

--- lib/widgets/scrollview.dart
+++ lib/widgets/scrollview.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class Scrollview extends StatefulWidget {
@@ -18,6 +20,55 @@ class ScrollviewState extends State<Scrollview> {
   ];
 
   String selectedButton = "Food";
+  List<String> carouselImages = [
+    'assets/images/baemin.png',
+  ];
+  List<String> carouselTitles = [
+    "BAEMIN",
+    "Title 3",
+    "Title 4",
+    "Title 5",
+    "Title 6",
+  ];
+  List<String> carousesublTitles = [
+    "Korea's No. 1 delivery app!",
+    "Title 3",
+    "Title 4",
+    "Title 5",
+    "Title 6",
+  ];
+
+  late PageController _pageController;
+  int _currentPage = 0;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _currentPage);
+    _startTimer();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 4), (_) {
+      if (_currentPage < carouselImages.length - 1) {
+        _currentPage++;
+      } else {
+        _currentPage = 0;
+      }
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -73,55 +124,67 @@ class ScrollviewState extends State<Scrollview> {
               fit: BoxFit.cover,
             ),
           ),
-          child: Stack(
-            children: [
-              Positioned(
-                bottom: 30,
-                left: 25,
-                child: Container(
-                  width: 300,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(30)),
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 55,
-                left: 55,
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/baemin.png'),
+          child: SizedBox(
+            width: 350,
+            height: 350,
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: carouselImages.length,
+              itemBuilder: (context, index) {
+                return Stack(
+                  children: [
+                    Positioned(
+                      bottom: 30,
+                      left: 25,
+                      child: Container(
+                        width: 300,
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30)),
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-              const Positioned(
-                bottom: 85,
-                left: 120,
-                child: Text(
-                  "BAEMIN",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const Positioned(
-                bottom: 65,
-                left: 120,
-                child: Text(
-                  "Korea's No. 1 delivery app!", //api
-                  style: TextStyle(
-                    color: Color(0xffA8A6A6),
-                    fontSize: 14,
-                    letterSpacing: 0,
-                  ),
-                ),
-              ),
-            ],
+                    Positioned(
+                      bottom: 55,
+                      left: 55,
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(carouselImages[index]),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 85,
+                      left: 120,
+                      child: Text(
+                        carouselTitles[index],
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 65,
+                      left: 120,
+                      child: Text(
+                        carousesublTitles[index],
+                        style: const TextStyle(
+                          color: Color(0xffA8A6A6),
+                          fontSize: 14,
+                          letterSpacing: 0,
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
           ),
         ),
         const SizedBox(height: 10),
